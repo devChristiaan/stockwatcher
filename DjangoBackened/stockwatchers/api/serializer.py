2a865a81b3d9fb4294b1f2934d6b8327e3ac8934
Christiaan Hougaard <cwhougaard@gmail.com>
2022-01-01 20:42:22 -0500

    added basic watchlist app and user app"

---
+++ DjangoBackened/stockwatchers/api/serializer.py
@@ -0,0 +1,15 @@
+from django.db.models import fields
+from rest_framework import serializers
+from stock.models import Stock
+from watchlist.models import Watchlist
+
+class StockSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Stock
+    fields = ['ticker', 'user']
+    
+    
+class WatchlistSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Watchlist
+    fields = ['name', 'user']
--- DjangoBackened/stockwatchers/stock/models.py
+++ DjangoBackened/stockwatchers/stock/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 from django.db.models.deletion import CASCADE
 
-# Create your models here.
-class Stocks (models.Model):
+# Stock Model.
+class Stock (models.Model):
   ticker = models.CharField(max_length=5, null=False, blank=False)
   user = models.CharField(max_length=25, null=False, blank=False)
-  watchlist_id = models.ForeignKey( , on_delete=models.CASCADE)
+  # watchlist_id = models.ForeignKey( , on_delete=models.CASCADE)
