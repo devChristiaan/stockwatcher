ed14d174ccc33d01a9c9ddb98e694b4444dda5d1
Christiaan Hougaard <cwhougaard@gmail.com>
2022-01-01 20:30:32 -0500

    added basic stock model

--- DjangoBackened/stockwatchers/stockwatchers/settings.py
+++ DjangoBackened/stockwatchers/stockwatchers/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'knox',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,10 @@ WSGI_APPLICATION = 'stockwatchers.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'stockwatchers',
+        'USER': 'root',
+        'PASSWORD': 'admin'
     }
 }
 
