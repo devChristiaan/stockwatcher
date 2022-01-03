7b9cd3f7b3148cc1cda943cdc5a45cb2b52e1df4
Christiaan Hougaard <cwhougaard@gmail.com>
2022-01-03 13:34:05 -0500

    added registration view

--- DjangoBackened/stockwatchers/accounts/serializers.py
+++ DjangoBackened/stockwatchers/accounts/serializers.py
@@ -10,5 +10,15 @@ class UserSerializer(serializers.ModelSerializer):
     fields = ('id', 'username', 'email')
     
 # Register Serializer
+class RegisterSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = User
+    fields = ('id', 'username', 'email', 'password')
+    extra_kwargs = { 'password': {'write_only': True} }
+    
+  def create(self, validated_data):
+    user = User.objects.create_user(validated_data['username'], validated_data['email'], validated_data['password'])
+    
+    return user
 
 # Login Serializer
