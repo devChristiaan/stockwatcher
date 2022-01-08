03d7528abad94f771d92782644b7b832b98b5c9b
Christiaan Hougaard <cwhougaard@gmail.com>
2022-01-08 16:18:25 -0500

    added redux store to app js and integrated the state with the app

--- react_frontend/src/App.js
+++ react_frontend/src/App.js
@@ -7,14 +7,19 @@ import {
 import HomePage from "./pages/HomePage";
 import NavBar from "./components/NavBar";
 
+import { Provider } from "react-redux";
+import store from "./store";
+
 function App() {
   return (
-    <Router>
-      <NavBar />
-      <Routes>
-        <Route element={<HomePage />} path="/" exact />
-      </Routes>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route element={<HomePage />} path="/" exact />
+        </Routes>
+      </Router>
+    </Provider>
   );
 }
 
